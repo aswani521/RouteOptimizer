6f177f9844c8c6c9f82e8e4de626a5ac86e1de15
Aswani Nerella <aswani521@gmail.com>
2015-12-06 23:50:04 -0800

    Add MapSearchResult & MerchantDetails.xib files

---
+++ RouteOptimizer/RouteOptimizer/MerchantDetailsView.h
@@ -0,0 +1,13 @@
+//
+//  MerchantDetailsView.h
+//  RouteOptimizer
+//
+//  Created by Aswani Nerella on 12/6/15.
+//  Copyright © 2015  Minett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MerchantDetailsView : UIViewController
+
+@end
